7c43c1a1cc67c862863946e14ba9fe31061df684
viralcryptoinsights-ux <viralcryptoinsights@gmail.com>
2026-01-07 15:38:29 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-
+document.addEventListener("scroll",()=>{
+  document.querySelectorAll(".reveal").forEach(el=>{
+    if(el.getBoundingClientRect().top < window.innerHeight - 80){
+      el.style.opacity=1;
+      el.style.transform="translateY(0)";
+    }
+  });
+});
